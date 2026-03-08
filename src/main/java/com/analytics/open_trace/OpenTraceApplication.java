743afc4d93bd3c4c4a145ca2ce59d6df53918f68
Yodieyogaming <groendyl@gmail.com>
2026-03-08 11:37:36 +0100

    initialize Project

---
+++ src/main/java/com/analytics/open_trace/OpenTraceApplication.java
@@ -0,0 +1,13 @@
+package com.analytics.open_trace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTraceApplication.class, args);
+	}
+
+}
